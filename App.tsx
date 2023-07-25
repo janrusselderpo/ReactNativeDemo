f0b80170b295b197b9e86ce7671d7135ac548721
janrusselderpo <janrusselderpo11@gmail.com>
2023-07-25 21:51:40 +0800

    react native demo

---
+++ App.tsx
@@ -0,0 +1,127 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React, {useEffect, useRef} from 'react';
+import {
+  AppState,
+  AppStateStatus,
+  Linking,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {MetaMaskSDK} from '@metamask/sdk';
+import {LogBox} from 'react-native';
+import BackgroundTimer from 'react-native-background-timer';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {DAPPView} from './src/views/DappView';
+
+LogBox.ignoreLogs([]); // Ignore log notification by message
+
+// TODO how to properly make sure we only try to open link when the app is active?
+// current problem is that sdk declaration is outside of the react scope so I cannot directly verify the state
+// hence usage of a global variable.
+let canOpenLink = true;
+
+const sdk = new MetaMaskSDK({
+  openDeeplink: (link: string) => {
+    if (canOpenLink) {
+      Linking.openURL(link);
+    }
+  },
+  timer: BackgroundTimer,
+  dappMetadata: {
+    url: 'ReactNativeTS',
+    name: 'ReactNativeTS',
+  },
+});
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  const [ready, setReady] = React.useState(false);
+  const shouldInit = useRef(true);
+
+  // initialize sdk
+  useEffect(() => {
+    if (!shouldInit.current) {
+      return;
+    }
+    shouldInit.current = false;
+
+    sdk.init().then(() => {
+      setReady(true);
+    });
+  });
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', handleAppState);
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
+  const handleAppState = (appState: AppStateStatus) => {
+    console.debug(`AppState change: ${appState}`);
+    canOpenLink = appState === 'active';
+  };
+
+  const backgroundStyle = {
+    backgroundColor: Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <View
+          // eslint-disable-next-line react-native/no-inline-styles
+          style={{
+            marginTop: 30,
+            backgroundColor: Colors.white,
+          }}>
+          <Text style={{color: Colors.black, fontSize: 24}}>
+            reactNativeDemo (RN v0.71.7)
+          </Text>
+          {ready ? <DAPPView sdk={sdk} /> : <Text>initializing...</Text>}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
